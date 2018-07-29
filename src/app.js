a053ebae4343392a07fe6f73a827303ae60f304e
sawhney133 <41874249+sawhney133@users.noreply.github.com>
2018-07-29 10:52:51 -0700

    Add files via upload
    Initial Checkin

---
+++ src/app.js
@@ -0,0 +1,159 @@
+'use strict';
+
+//Function to right pad the string
+function right_pad(Str, Len) {
+
+    Str = String(Str);
+    let i = 0;
+    Len = Len - Str.length;
+    while (i < Len) {
+        Str = Str + " ";
+
+        i++;
+    }
+    return Str;
+}
+
+//Funtion to check input string matches desired schema and valid values and valid datatypes
+function is_valid_object(Element) {
+
+    if (Element.constructor == Object) {
+
+        return (Object.keys(Element).length == 2 &&
+            Object.keys(Element).indexOf('Name') >= 0 &&
+            typeof Element.Name == 'string' &&
+            Object.keys(Element).indexOf('Details') >= 0 &&
+            Element.Details.constructor == Object &&
+            Object.keys(Element.Details).length == 2 &&
+            Object.keys(Element.Details).indexOf('Type') >= 0 &&
+            Object.keys(Element.Details).indexOf('Weight') >= 0 &&
+            (Element.Details.Type == 'l' || Element.Details.Type == 'k') &&
+            typeof Element.Details.Weight == 'number'
+        );
+    }
+    return false;
+}
+
+//Function to validate input values
+function validate_input(Original_Object) {
+
+    if (Original_Object.constructor == Object && is_valid_object((Original_Object))) {
+
+        return [(Original_Object)];
+
+    }
+    else if (Original_Object.constructor == Array && Original_Object.length > 0) {
+        for (let i = 0; i < Original_Object.length; i++) {
+
+            if (is_valid_object(Original_Object[i])) {
+
+                continue;
+
+            }
+            else {
+
+                return false;
+            }
+
+        }
+
+        return Original_Object;
+    }
+
+    return false;
+}
+
+//Function to convert an element lb to kg
+function convert_to_k(Original_Object) {
+
+    var Converted_Object = Original_Object;
+    Converted_Object.Details.Type = "k";
+    Converted_Object.Details.Weight *= 0.45359237;
+
+    return Converted_Object;
+
+}
+
+//Function to convert a list from lb to kg
+function convert_list_to_k(List_of_objects) {
+
+    var Converted_List = [];
+
+    List_of_objects.forEach(function (Original_Object) {
+
+        //If weight type is lb then convert it to kg
+        Original_Object.Details.Type == 'l' ? Converted_List.push(convert_to_k(Original_Object)) : Converted_List.push(Original_Object);
+
+    })
+
+    return Converted_List;
+
+}
+
+//Function to find minimum and maximum weight
+function find_max_and_min(List) {
+
+    let Sorted_List = List;
+
+    //Bubble Sort
+    for (let i = 0; i < Sorted_List.length; i++) {
+
+        for (let j = i + 1; j < Sorted_List.length; j++) {
+            if (Sorted_List[i].Details.Weight > Sorted_List[j].Details.Weight) {
+
+                let swap = Sorted_List[i];
+                Sorted_List[i] = Sorted_List[j];
+                Sorted_List[j] = swap;
+
+            }
+        }
+    }
+
+    return [Sorted_List[0], Sorted_List[Sorted_List.length - 1]];
+}
+
+//Formatted print on console
+function print_weight(Converted_List) {
+
+    Converted_List.forEach(function (Converted_Object) {
+
+        console.log(right_pad(Converted_Object.Name, 15), Converted_Object.Details.Weight);
+    })
+}
+
+//Print maximum and minimum weight
+function print_max_and_min(Min, Max) {
+
+    console.log("Max weight was %s c in %s", JSON.stringify(Max.Details), Max.Name);
+    console.log("Min weight was %s c in %s", JSON.stringify(Min.Details), Min.Name);
+}
+
+//Function to convert list weight to kg and find minimum/maximum weight in the list -
+module.exports.format_weight = function format_weight(List_of_objects) {
+    try {
+        let Valid_Input = validate_input(List_of_objects);
+
+        if (Valid_Input) {
+
+            var Converted_List = convert_list_to_k(Valid_Input);
+
+            print_weight(Converted_List);
+
+            var result = find_max_and_min(Converted_List);
+
+            print_max_and_min(result[0], result[1]);
+
+            return result;
+        }
+        else {
+
+            console.log("Invalid Input", List_of_objects);
+            return ("Invalid Input");
+        }
+    }
+    catch (error) {
+
+        console.log("Unexpected Error!", error)
+        return ("Unexpected Error!", error)
+    }
+}
---
+++ test/service/ServiceValidationTest.js
@@ -0,0 +1,144 @@
+'use strict';
+
+//Import modules
+const chai = require('chai');
+const expect = chai.expect;
+const testData = require('./test_data.json');
+const servicefunctions = require('../../src/app.js');
+
+if (testData == null) {
+    console.error("Test data not found.");
+}
+
+describe('Invalid Input', () => {
+
+    it('Blank array', (done) => {
+        var res = servicefunctions.format_weight(testData['improperRequests']['blankArray']['request']);
+        expect(res).equals(testData['improperRequests']['blankArray']['response']);
+        done();
+    });
+
+    it('Blank json', (done) => {
+        var res = servicefunctions.format_weight(testData['improperRequests']['blankJson']['request']);
+        expect(res).equals(testData['improperRequests']['blankJson']['response']);
+        done();
+    });
+
+    it('Incorrect schema passed - less than two elements under root', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['oneElementUnderRoot']['request']);
+        expect(res).equals(testData['improperRequests']['oneElementUnderRoot']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - more than two elements under root', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['threeElementsUnderRoot']['request']);
+        expect(res).equals(testData['improperRequests']['threeElementsUnderRoot']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Name missing', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['nameMissing']['request']);
+        expect(res).equals(testData['improperRequests']['nameMissing']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Name Type not string', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['nameIsNum']['request']);
+        expect(res).equals(testData['improperRequests']['nameIsNum']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Details Missing', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['detailsMissing']['request']);
+        expect(res).equals(testData['improperRequests']['detailsMissing']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Details Type not Object', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['detailsIsNum']['request']);
+        expect(res).equals(testData['improperRequests']['detailsIsNum']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - less than two elements under Details', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['oneElementUnderDetails']['request']);
+        expect(res).equals(testData['improperRequests']['oneElementUnderDetails']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - more than two elements under Details', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['threeElementsUnderDetails']['request']);
+        expect(res).equals(testData['improperRequests']['threeElementsUnderDetails']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Type is missing', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['typeMissing']['request']);
+        expect(res).equals(testData['improperRequests']['typeMissing']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - Type is invalid', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['typeIsInvalid']['request']);
+        expect(res).equals(testData['improperRequests']['typeIsInvalid']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - weight is missing', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['weightMissing']['request']);
+        expect(res).equals(testData['improperRequests']['weightMissing']['response']);
+        done();
+
+    });
+
+    it('Incorrect schema passed - weight is invalid', (done) => {
+
+        var res = servicefunctions.format_weight(testData['improperRequests']['weightIsInvalid']['request']);
+        expect(res).equals(testData['improperRequests']['weightIsInvalid']['response']);
+        done();
+
+    });
+
+});
+describe('Valid Input', () => {
+
+    it('Single Element passed', (done) => {
+
+        var res = servicefunctions.format_weight(testData['properRequests']['singleElement']['request']);
+        expect(res[0].Details.Weight).equals(testData['properRequests']['singleElement']['response']['MinWeight']);
+        expect(res[1].Details.Weight).equals(testData['properRequests']['singleElement']['response']['MaxWeight']);
+        done();
+
+    });
+
+    it('Multiple Element passed', (done) => {
+
+        var res = servicefunctions.format_weight(testData['properRequests']['multipleElement']['request']);
+        expect(res[0].Details.Weight).equals(testData['properRequests']['multipleElement']['response']['MinWeight']);
+        expect(res[1].Details.Weight).equals(testData['properRequests']['multipleElement']['response']['MaxWeight']);
+        done();
+
+    });
+
+});
